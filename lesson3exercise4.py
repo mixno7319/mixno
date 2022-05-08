b0f7df756ce521607f538e340cd43508fc6912a9
Mixno <44552200s@gmail.com>
2022-05-08 14:47:28 +0300

    install commit

---
+++ lesson3exercise4.py
@@ -0,0 +1,11 @@
+a = int(input('Введите значение числа a = '))
+b = int(input('Введите значение числа b = '))
+c = int(input('Введите значение числа c = '))
+summ = 0;
+if a > 0:
+    summ = summ + 1
+if b > 0:
+    summ = summ + 1
+if c > 0:
+    summ = summ + 1
+print('Количество положительных  чисел: ', summ)
